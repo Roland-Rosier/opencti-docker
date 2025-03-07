5d23a9c192ad29d342fa3efe91fb9ede5da684c7
Roland Rosier <201844637+Roland-Rosier@users.noreply.github.com>
2025-03-08 00:04:37 +0100

    Non-working configuration for proxmox-lxc

---
+++ dockerfiles/redis/docker-entrypoint.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -e
+
+# WARNING overcommit_memory is set to 0! Background save may fail under low memory condition.
+# To fix this issue add 'vm.overcommit_memory = 1' to /etc/sysctl.conf and then reboot
+# or run the command 'sysctl vm.overcommit_memory=1' for this to take effect.
+# The overcommit_memory has 3 options.
+# 0, the system kernel check if there is enough memory to be allocated to the process or not,
+# if not enough, it will return errors to the process.
+# 1, the system kernel is allowed to allocate the whole memory to the process
+# no matter what the status of memory is.
+# 2, the system kernel is allowed to allocate a memory whose size could be bigger than
+# the sum of the size of physical memory and the size of exchange workspace to the process.
+sysctl vm.overcommit_memory=1
+
+# first arg is `-f` or `--some-option`
+# or first arg is `something.conf`
+if [ "${1#-}" != "$1" ] || [ "${1%.conf}" != "$1" ]; then
+        set -- redis-server "$@"
+fi
+
+# allow the container to be started with `--user`
+if [ "$1" = 'redis-server' -a "$(id -u)" = '0' ]; then
+        find . \! -user redis -exec chown redis '{}' +
+        exec gosu redis "$0" "$@"
+fi
+
+# set an appropriate umask (if one isn't set already)
+# - https://github.com/docker-library/redis/issues/305
+# - https://github.com/redis/redis/blob/bb875603fb7ff3f9d19aad906bd45d7db98d9a39/utils/systemd-redis_server.service#L37
+um="$(umask)"
+if [ "$um" = '0022' ]; then
+        umask 0077
+fi
+
+exec "$@"
---
+++ dockerfiles/redis/empty-docker-entrypoint.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+set -e
+
+# set an appropriate umask (if one isn't set already)
+# - https://github.com/docker-library/redis/issues/305
+# - https://github.com/redis/redis/blob/bb875603fb7ff3f9d19aad906bd45d7db98d9a39/utils/systemd-redis_server.service#L37
+um="$(umask)"
+if [ "$um" = '0022' ]; then
+        umask 0077
+fi
+
+if [ $# -gt 0 ] ; then
+  exec "$@"
+fi
